9555c63fd35b13a1543e43feb0c1c90d25bdc263
shakedN <shnuttman@gmail.com>
2026-01-10 15:50:50 +0200

    remove header

--- client/src/layouts/RootLayout.jsx
+++ client/src/layouts/RootLayout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header/Header.jsx";
 
 export default function RootLayout() {
   return (
     <>
-      <Header />
       <main style={{ padding: 16 }}>
         <Outlet />
       </main>
